98025c4aa60bc7b915be6fb209fb2787fbb8d35e
scapig@yandex.ru <scapig@yandex.ru@7765641d-c9e6-99aa-21d5-4ed59f5fbf26>
2011-12-20 09:27:46 +0000

    mio module: Fix comments.

--- trunk/src/mio/mio.cpp
+++ trunk/src/mio/mio.cpp
@@ -2,25 +2,27 @@
 Project :
 Version :
 Date    : 30.03.2011
-Author  : Шиенков Д.И.
+Author  : Denis Shienkov 
 Company :
 Comments:
 License : New BSD
 **********************************************************************************************/
 
 /*! \file mio.cpp
-
-    Аббревиатура модуля (файла) "mio" - Module Input Output.
-
-    Здесь реализован API для доступа к модулям локального I/O ПЛК серии ADAM 5510. \n
-
-    Модули I/O ПЛК делятся на высокоскоростные и низкоскоростные.
-    Получение данных из высокоскоростных модулей производится напрямую путем чтения портов этих модулей
-    по базовым адресам и соответствующих их смещениям.
-    Получение данных из низкоскоростных модулей производится путем обмена командами с модулем по
-    через внутреннюю UART шину модуля по протоколу типа DCON.
-
-*/
+ *
+ * Abbreviation of the module (file) "mio" - Module Input Output.
+ *
+ * It implements an API to access the local I/O modules a PLC ADAM 5510. 
+ *
+ * Modules I/O PLCs are divided into high and low speed.
+ * 
+ * Getting data from high-speed modules made ​​directly by reading the ports of these modules
+ * on the base address and relevant to their shifts.
+ * 
+ * Getting data from low-speed modules is through the exchange of commands to the module on
+ * through the internal bus of the module UART protocol type DCON.
+ *
+ */
 
 #include <dos.h>
 #include <conio.h>
@@ -28,56 +30,68 @@ License : New BSD
 
 
 //--------------------------------------------------------------------------------------------------------//
-/*** ЛОКАЛЬНЫЕ ПЕРЕМЕННЫЕ ***/
+/*** Private variables ***/
 
-/*! Базовые адреса слотов 0 - 7 ПЛК. \n
-    Исходя из анализа декомпилированного ассемблерного кода (с помощью IDA)
-    библиотек от Advantech видно, что каждый из 8 слотов для посадки модуля имеет
-    свой базовый адрес (например, слот №0 - адрес 0х0100).
-    Базовые адреса модулей чередуются через 16 регистров, т.е. каждый модуль
-    имеет теоретически по 16 регистров (адресов, смещений) ввода/вывода. \n
-    Для каждого конкретного типа модуля назначение этих регистров может быть разное
-    и это назначение можно узнать проанализировав декомпилированный ассемблерный код библиотек
-    от Advantech!
-*/
+/*! 
+ * Base addresses of slots 0 - 7 PLC. 
+ * 
+ * Based on the analysis of the decompiled assembler code (using IDA)
+ * libraries from Advantech is clear that each of the 8 slots for landing module has
+ * its base address (for example, the slot number 0 - 0x0100 address).
+ * 
+ * Alternate base addresses of modules through 16 registers, ie, each module
+ * theoretically has 16 registers (address, offset) input/output. For each specific
+ * type of module function of these registers may be different and this designation
+ * can be found analyzing the decompiled assembly code libraries from Advantech!
+ */
 static const int board_base[8] = { 0x0100, 0x0110, 0x0120, 0x0130, 0x0140, 0x0150, 0x0160, 0x0170 };
 
-/*! Битовые маски каждого бита в байте,
-    используются просто для оптимизации алгоитмов и кода.
-*/
+/*! 
+ * Bitmasks of each bit in a byte, simply used to optimize the 
+ * algorithms and code.
+ */
 static const char bit_mask[8] = { 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80 };
 
+/*!
+ * Unknown the destination.
+ */
 static char can_xxx_proc[8] = { 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01, 0x01 };
 
-/*! Массив из 8-ми элементов, которые являются предыдущими
-    значениями каналов дискретных модулей ввода или вывода в слотах 0 - 7:
-    - при получении текущих значений каналов с модулей DI, эти текущие значения сразу
-    сохраняются в массив (хз для чего это сделано, нужно подумать)
-    - при выдаче управления каналами в модули DO, желаемые значения каналов сохраняются
-    в массив (это типа нужно чтобы косвенно определить текущее состояние каналов модуля
-    DO, т.к. нет? возможности определить программно это состояние прочитав регистры модуля!).
-    .
-*/
+/*! 
+ * An array of 8 elements, which are the previous values ​​of channel discrete input
+ * or output modules in slots 0 - 7:
+ * - If the current values ​​of channel modules with DI, these current values ​​at once
+ * stored in the array (for which this is done??? you need to think!!!)
+ * - The issuance of channel management modules in DO, the desired channel values ​​are stored
+ * in the array (this is the type needed to indirectly determine the current state of the channel module
+ * DO, as no? possible to determine the software is able to read the registers of the module!).
+ */
 static int dio_val[8] = { 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000 };
 
 //--------------------------------------------------------------------------------------------------------//
-/*** ЛОКАЛЬНЫЕ ФУНКЦИИ ***/
+/*** Private functions ***/
 
 //--------------------------------------------------------------------------------------------------------//
-/*** ГЛОБАЛЬНЫЕ ПЕРЕМЕННЫЕ ***/
+/*** Public variables ***/
 
 //--------------------------------------------------------------------------------------------------------//
-/*** ГЛОБАЛЬНЫЕ ФУНКЦИИ ***/
+/*** Global functions ***/
 
 /*!
-*/
+ */
 void get_di_val(int slot, int *val)
 {
-    *val = (inp(board_base[slot] + 3) << 8) | inp(board_base[slot] + 2);//получили текущее значение +2/+3
-    if (can_xxx_proc[slot]) {//если функцию вызвали впервые то читаем регистры +4/+5
-        can_xxx_proc[slot] = 0;//делаем так чтобы при след. вызове не читать ХЗ регистры
+    // Get current value at offset +2 and +3.
+    *val = (inp(board_base[slot] + 3) << 8) | inp(board_base[slot] + 2);
+    
+    // If the function is called first then read the registers at offset +4 and +5
+    // NOTE: The purpose of these registers have not yet been elucidated!
+    if (can_xxx_proc[slot]) {
+    
+        // The next call to not read the registers at offsets +4 and +5.
+        can_xxx_proc[slot] = 0;
 
-        // Занимаемся преобразованием значений из ХЗ регистров +4/+5
+        // Transform the values ​​of registers at offsets +4 and +5.
         {
             dio_val[slot] = ((~(inp(board_base[slot] + 5))) << 8) | (~(inp(board_base[slot] + 4)));
             int old_val = 0;
@@ -94,10 +108,10 @@ void get_di_val(int slot, int *val)
 }
 
 /*! 
-*/
-// По 1 биту устанавливаем или сбрасываем!!!
+ */
 void set_do_bit(int slot, int bit, int val)
 {
+    // One bit set or reset.
     if ((8 > slot) && (16 > bit)) {
         if (val) { dio_val[slot] |= (1 << bit); }
         else { dio_val[slot] &= ~(1 << bit); }
